3125a39deb82c889c661476977dd96f3a66416ff
William McCusker <Smurf1.bm@gmail.com>
2023-05-15 20:22:03 +0100

    Constructed basic sidebar

---
+++ Main.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" href="css/main.css">
+
+<div class="sidebar">
+    <p class="current">Home</p>
+    <div class="divider"></div>
+    <a href="Main.php">Home</a>
+    <div class="line"></div>
+    <a href="locations.php">Locations</a>
+    <div class="line"></div>
+    <a href="cost management.php">Cost management</a>
+    <div class="line"></div>
+    <a href="menu.php">Menu</a>
+    <div class="line"></div>
+    <a href="profit.php">Profit</a>
+    <div class="line"></div>
+    <a href="workflow.php">Workflow</a>
+    <div class="line"></div>
+</div>
